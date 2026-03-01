e4c2b239647527507f410cab25a5796d13e6c0c7
chiru447 <av2193@srmist.edu.in>
2026-03-01 23:23:35 +0530

    feature/UC1:

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -4,3 +4,25 @@ public class OOPSBannerApp {
         System.out.println("OOPS");
     }
 }
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+
+            " *****    *****    *****    *****  ",
+            "*     *  *     *  *     *  *       ",
+            "*     *  *     *  *     *  *       ",
+            "*     *  *     *  *****     *****  ",
+            "*     *  *     *  *               *",
+            "*     *  *     *  *               *",
+            " *****    *****    *         *****  "
+
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
